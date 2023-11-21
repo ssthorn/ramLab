0aacc6342267324a796cd75fecc4194d273f9198
Sam <sam.stearn@gmail.com>
2023-11-20 18:39:53 -0800

    token update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
 import CharacterContainer from './characterContainer/characterContainer';
-import SingleCharacterComponent from './characterContainer/singleCharacterComponent/singleCharacterComponent';
 
 function App() {
   return (
